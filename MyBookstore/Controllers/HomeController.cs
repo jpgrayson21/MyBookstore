734bcb20c33d9a1d36be5ab7d1d2e0720ddb1cbf
James Grayson <jaygray@byu.edu>
2022-02-16 23:16:25 -0700

    Add project files.

---
+++ MyBookstore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBookstore.Models;
+using MyBookstore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        //takes input of the pageNum so that the action knows which results to return
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+            return View(x);
+        }
+
+    }
+}
---
+++ MyBookstore/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBookstore.Models.ViewModels
+{
+    public class PageInfo
+    {
+        /*necessary info to produce page index at bottom of page*/
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //Figure out how many pages we need
+        public int TotalPages => (int)Math.Ceiling((double)TotalNumBooks / BooksPerPage);
+    }
+}
